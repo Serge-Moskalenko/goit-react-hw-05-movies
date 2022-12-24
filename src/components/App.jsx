50c9ed6720d8f0b0905ecf94d8b5c412c116a92e
Serge-Moskalenko <skmos2019@outlook.com>
2022-12-24 12:58:19 +0000

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,25 @@
+import { Routes, Route } from "react-router-dom";
+import { Home } from './Home/Home';
+
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      {/* <Route path='/movies' element={Movies} />
+      <Route path='/movies/:movieId' element={MovieDetails} >
+        <Route path='cast' element={Cast} />
+        <Route path='reviews' element={Reviews} />
+      </Route> */}
+    </Routes>
   );
 };
+
+  // api_key= '411d08d89a4569fb1b50aec07ee6fb72'
+
+
+// '/' - компонент Home, домашняя страница со списком популярных кинофильмов.
+// '/movies' - компонент Movies, страница поиска фильмов по ключевому слову.
+// '/movies/:movieId' - компонент MovieDetails, страница с детальной информацией о кинофильме.
+// /movies/:movieId/cast - компонент Cast, информация о актерском составе. Рендерится на странице MovieDetails.
+// /movies/:movieId/reviews - компонент Reviews, информация об обзорах. Рендерится на странице MovieDetails.
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,15 @@
+export const Home = () => {
+    return (
+        <main>
+            <header>
+                <nav>
+                    Home Movies
+                </nav>
+            </header>
+
+            <section>
+                <h1>Trending todey</h1>
+            </section>
+        </main>
+    )
+}
